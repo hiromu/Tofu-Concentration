fa8a26d6eae9735a017ea4c88eae13179e2ce5c7
Hiromu Yakura <hiromu1996@gmail.com>
2014-10-19 23:47:15 +0900

    add timer and mistake counter

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var highlight = null;
+var mistake = 0;
+var start = null;
+var timer = null;
 
 chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
 	if(request.launch) {
@@ -25,6 +28,8 @@ chrome.extension.onMessage.addListener(function (request, sender, sendResponse) 
 						$(this).text($(this).attr('tofu-character')).removeClass('tofu-hidden').unbind('click');
 					});
 				} else {
+					mistake += 1;
+					$('#tofu-mistake').text('mistake: ' + mistake);
 					$(event.target).text($(event.target).attr('tofu-character'));
 					
 					var target = [highlight, event.target];
@@ -36,11 +41,31 @@ chrome.extension.onMessage.addListener(function (request, sender, sendResponse) 
 				highlight = null;
 			}
 		});
+		
+		$('body').append('\
+			<div id="tofu-scoreboard" style="position: fixed; top: 20px; right: 20px;">\
+				<p id="tofu-clock" style="color: #ffa500; font-size: 25px; text-align: center;">00:00</p>\
+				<p id="tofu-mistake" style="color: #a52175; font-size: 18px; text-align: center;">mistake: 0</p>\
+			</div>\
+		');
+		
+		mistake = 0;
+		start = new Date();
+		timer = setInterval(updateTimer, 1000);
 	} else {
 		$('.tofu-hidden').each(function() {
 			$(this).text($(this).attr('tofu-character')).removeClass('tofu-hidden').unbind('click');
 		});
+		$('#tofu-scoreboard').remove();
+		clearTimeout(timer);
 	}
 	
 	sendResponse();
 });
+
+function updateTimer() {
+	var diff = Math.floor((new Date() - start) / 1000);
+	var minutes = Math.floor(diff / 60) < 10? '0' + Math.floor(diff / 60): Math.floor(diff / 60);
+	var seconds = diff % 60 < 10? '0' + diff % 60: diff % 60;
+	$('#tofu-clock').text(minutes + ':' + seconds);
+}
